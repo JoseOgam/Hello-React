1cb9e40bfcdafdc9410dc2261655d244068838f0
JoseOgam <jotienoochieng@gmail.com>
2020-07-10 15:08:24 -0400

    visibility done with webpack

--- public/bundle.js
+++ public/bundle.js
@@ -497,6 +497,8 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(2);
 
 var _react2 = _interopRequireDefault(_react);
@@ -507,21 +509,72 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// console.log("react is great");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } // console.log("react is great");
 
 // import add from "./app2";
 
 // console.log(add(1, 2, 3));
 
-var MyData = function MyData() {
-  return _react2.default.createElement(
-    "h1",
-    null,
-    "Reading non-fiction work"
-  );
-};
+// var MyData = () => {
+//   return <h1>Reading non-fiction work</h1>;
+// };
+var ToggleVisible = function (_React$Component) {
+  _inherits(ToggleVisible, _React$Component);
 
-_reactDom2.default.render(_react2.default.createElement(MyData, null), document.getElementById("app"));
+  function ToggleVisible(props) {
+    _classCallCheck(this, ToggleVisible);
+
+    var _this = _possibleConstructorReturn(this, (ToggleVisible.__proto__ || Object.getPrototypeOf(ToggleVisible)).call(this, props));
+
+    _this.toggleButton = _this.toggleButton.bind(_this);
+    _this.state = {
+      Visibility: false
+    };
+    return _this;
+  }
+
+  _createClass(ToggleVisible, [{
+    key: "toggleButton",
+    value: function toggleButton() {
+      this.setState(function (prevState) {
+        return {
+          Visibility: !prevState.Visibility
+        };
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement(
+        "div",
+        null,
+        _react2.default.createElement(
+          "h1",
+          null,
+          "Visibility Toggle"
+        ),
+        _react2.default.createElement(
+          "button",
+          { onClick: this.toggleButton },
+          this.state.Visibility ? "Hide details" : "Show details"
+        ),
+        this.state.Visibility && _react2.default.createElement(
+          "p",
+          null,
+          "Hi. here are your details you can now see"
+        )
+      );
+    }
+  }]);
+
+  return ToggleVisible;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(ToggleVisible, null), document.getElementById("app"));
 
 /***/ }),
 /* 6 */
--- src/testdata/app1.js
+++ src/testdata/app1.js
@@ -7,8 +7,35 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-var MyData = () => {
-  return <h1>Reading non-fiction work</h1>;
-};
+// var MyData = () => {
+//   return <h1>Reading non-fiction work</h1>;
+// };
+class ToggleVisible extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleButton = this.toggleButton.bind(this);
+    this.state = {
+      Visibility: false,
+    };
+  }
+  toggleButton() {
+    this.setState((prevState) => ({
+      Visibility: !prevState.Visibility,
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.toggleButton}>
+          {this.state.Visibility ? "Hide details" : "Show details"}
+        </button>
+        {this.state.Visibility && (
+          <p>Hi. here are your details you can now see</p>
+        )}
+      </div>
+    );
+  }
+}
 
-ReactDom.render(<MyData />, document.getElementById("app"));
+ReactDom.render(<ToggleVisible />, document.getElementById("app"));
